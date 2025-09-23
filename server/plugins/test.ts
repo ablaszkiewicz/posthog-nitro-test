5405f5bce76b9ea0d045aabac83f4c338d62fe68
ablaszkiewicz <kqmdjc8@gmail.com>
2025-09-23 14:59:55 +0200

    feat: wip

--- server/plugins/test.ts
+++ server/plugins/test.ts
@@ -1,8 +1,4 @@
 import { defineNitroPlugin } from "nitropack/runtime";
 import { PosthogNitroAppPlugin } from "../random-directory/plugin-code";
 
-export default defineNitroPlugin(
-  PosthogNitroAppPlugin({
-    a: 5,
-  })
-);
+export default defineNitroPlugin(PosthogNitroAppPlugin);
